6525700a1fbda94a223bf79c5f79733c5b979842
Elijah Allen <56412012+eaallen@users.noreply.github.com>
2020-05-02 14:41:48 -0600

    added more firestore db methods

--- src/comps/Firebase/Firebase.js
+++ src/comps/Firebase/Firebase.js
@@ -35,6 +35,9 @@ export const AppContext = React.createContext()
             getOneRecord:this.getOneRecord,
             checkState: this.checkState,
             user: this.user,
+            doGetAllRecords: this.doGetAllRecords,
+            doGetTaskByCustomerID: this.doGetTaskByCustomerID,
+            
           }
           this.state = {
             test:'this is comming from the firbase context provider',
@@ -78,6 +81,32 @@ export const AppContext = React.createContext()
         doAddRecord = (_collection) => this.db.collection(_collection).doc();
         doGetQueryRecord = (_collection, item_looking_for,filtering_item) => this.db.collection(_collection).where(item_looking_for, '==',filtering_item).get();
         getOneRecord = (_collection, item_wanted) => this.db.collection(_collection).doc(item_wanted)
+        doGetTaskByCustomerID = async(_collection,id)=>{
+          let task_history = [] 
+          let tasks = await this.db.collection(`customers/${id}/tasks`).get()
+          for(const task of tasks.docs){
+            console.log('TASK----->',task.data())
+            let obj = task.data()
+            obj.id=task.id
+            task_history.push(obj)
+          }
+          this.setState({...this.state, tasks_of_current_customer: task_history})
+          return(task_history)
+        }
+          //Come back to this later---------------------------------------------------
+        async doGetAllRecords(_collection){
+          //get documents from 'customers' collection
+          let querySnapshot = await this.db.collection(_collection).get()
+          let arr = []
+          //for each 'key:value pair' . . . 
+          for(const doc of querySnapshot.docs){
+            let data = doc.data();
+            data['id'] = doc.id;
+            data.task_history= []
+            arr.push(data)
+          }
+          this.setState({...this.state, data:arr})       
+        }
         checkState = async() =>{ await
           this.auth.onAuthStateChanged(function(user) {
             if (user){
@@ -92,6 +121,41 @@ export const AppContext = React.createContext()
         loader=()=>{          
           this.setState({...this.state, loading:true})
         }
+        async loadFakeData(){
+          const names =  [['Qabil','Fabiana'],['Fabiano','Qacha'],['Qadan','Fabiola'],['Fabrice','Qadir'],['Qadr','Fabunni'],['Facebook','Qamar']]
+          // let cust = this.db.collection("customers").doc()
+          // console.log('YEEET',cust.id)
+          for(let name of names){
+            // make a customer instance
+            let cust = this.db.collection("customers").doc()
+            //make data instance inside customer
+            let tasks = this.db.collection(`customers/${cust.id}/tasks`)
+            //task data fir customer
+            tasks.add({
+              start_date:firebase.firestore.Timestamp.fromDate(new Date("December 10, 1815")),
+              end_date:firebase.firestore.Timestamp.fromDate(new Date("December 12, 1815")),
+              charge:"$20.00",
+              task_desc:'cleaning',
+            })
+            tasks.add({
+              start_date:firebase.firestore.Timestamp.fromDate(new Date("August 20, 1830")),
+              end_date:firebase.firestore.Timestamp.fromDate(new Date("September 1, 1830")),
+              charge:"$100.00",
+              task_desc:'repair',
+            })    
+            //customer data
+            cust.set({
+              first_name: name[0],
+              last_name: name[1],
+              phone_number: "123 321 1232",
+              email_address: 'san@fake.come',
+              last_in: firebase.firestore.Timestamp.fromDate(new Date("September 1, 1830")),
+              recent_task: 'repair',
+              notes: 'Nullam commodo eros ut commodo aliquam. Cras vestibulum accumsan bibendum. Morbi tristique massa a elit vehicula pellentesque. Nam iaculis posuere dui eu fermentum. Quisque in lectus leo. Aenean libero nunc, rutrum quis velit vel, tristique vulputate magna. Sed et lorem et lectus tempus dignissim.',
+              date_purchased: firebase.firestore.Timestamp.fromDate(new Date("December 26, 1777")),
+            })
+          }
+      }
       async componentDidMount(){
       // STUFF YOU DO RIGHT AT THE BEGINING 
       }
