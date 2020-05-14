3ccbe69a57165f94944c33d8ec5289d1d12ebfc9
eaallen <elijah.a.allen@gmail.com>
2020-05-14 09:54:29 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 import {withFirebase} from './comps/Firebase'
 function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {props.context.test}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Elijah Allen</h1>
+        <h3>Coding Challenge</h3>
+      </div>
+      
     </div>
   );
 }
--- src/comps/Firebase/Firebase.js
+++ src/comps/Firebase/Firebase.js
@@ -2,18 +2,18 @@ import app from 'firebase/app';
 import 'firebase/auth'
 import 'firebase/firestore'
 import React from 'react' 
+import axios from 'axios'
 export const AppContext = React.createContext()
 
     const config = {
-      // your config info here
-      // apiKey: ,
-      // authDomain: ,
-      // databaseURL: ,
-      // projectId: ,
-      // storageBucket: ,
-      // messagingSenderId: ,
-      // appId:,
-      // measurementId: 
+      apiKey: "AIzaSyAnjHEXFnx3xvl2g6pe5NQBpg_S5cfspsQ",
+      authDomain: "elijahallenlode.firebaseapp.com",
+      databaseURL: "https://elijahallenlode.firebaseio.com",
+      projectId: "elijahallenlode",
+      storageBucket: "elijahallenlode.appspot.com",
+      messagingSenderId: "801751025632",
+      appId: "1:801751025632:web:6795ff4665773fecd28d85",
+      measurementId: "G-TF67PQL8TY"
     };
    
     class Firebase extends React.Component {
@@ -81,85 +81,32 @@ export const AppContext = React.createContext()
         doAddRecord = (_collection) => this.db.collection(_collection).doc();
         doGetQueryRecord = (_collection, item_looking_for,filtering_item) => this.db.collection(_collection).where(item_looking_for, '==',filtering_item).get();
         getOneRecord = (_collection, item_wanted) => this.db.collection(_collection).doc(item_wanted)
-        doGetTaskByCustomerID = async(_collection,id)=>{
-          let task_history = [] 
-          let tasks = await this.db.collection(`customers/${id}/tasks`).get()
-          for(const task of tasks.docs){
-            console.log('TASK----->',task.data())
-            let obj = task.data()
-            obj.id=task.id
-            task_history.push(obj)
-          }
-          this.setState({...this.state, tasks_of_current_customer: task_history})
-          return(task_history)
-        }
-          //Come back to this later---------------------------------------------------
-        async doGetAllRecords(_collection){
-          //get documents from 'customers' collection
-          let querySnapshot = await this.db.collection(_collection).get()
-          let arr = []
-          //for each 'key:value pair' . . . 
-          for(const doc of querySnapshot.docs){
-            let data = doc.data();
-            data['id'] = doc.id;
-            data.task_history= []
-            arr.push(data)
-          }
-          this.setState({...this.state, data:arr})       
-        }
-        checkState = async() =>{ await
-          this.auth.onAuthStateChanged(function(user) {
-            if (user){
-              console.log('user accorfing to firebase')
-            }else{
-              console.log('according to firebase: no user info')
-            }    
-          });
-        }
-        user = () => this.auth.currentUser
-    
-        loader=()=>{          
-          this.setState({...this.state, loading:true})
-        }
-        async loadFakeData(){
-          const names =  [['Qabil','Fabiana'],['Fabiano','Qacha'],['Qadan','Fabiola'],['Fabrice','Qadir'],['Qadr','Fabunni'],['Facebook','Qamar']]
-          // let cust = this.db.collection("customers").doc()
-          // console.log('YEEET',cust.id)
-          for(let name of names){
-            // make a customer instance
-            let cust = this.db.collection("customers").doc()
-            //make data instance inside customer
-            let tasks = this.db.collection(`customers/${cust.id}/tasks`)
-            //task data fir customer
-            tasks.add({
-              start_date:firebase.firestore.Timestamp.fromDate(new Date("December 10, 1815")),
-              end_date:firebase.firestore.Timestamp.fromDate(new Date("December 12, 1815")),
-              charge:"$20.00",
-              task_desc:'cleaning',
-            })
-            tasks.add({
-              start_date:firebase.firestore.Timestamp.fromDate(new Date("August 20, 1830")),
-              end_date:firebase.firestore.Timestamp.fromDate(new Date("September 1, 1830")),
-              charge:"$100.00",
-              task_desc:'repair',
-            })    
-            //customer data
-            cust.set({
-              first_name: name[0],
-              last_name: name[1],
-              phone_number: "123 321 1232",
-              email_address: 'san@fake.come',
-              last_in: firebase.firestore.Timestamp.fromDate(new Date("September 1, 1830")),
-              recent_task: 'repair',
-              notes: 'Nullam commodo eros ut commodo aliquam. Cras vestibulum accumsan bibendum. Morbi tristique massa a elit vehicula pellentesque. Nam iaculis posuere dui eu fermentum. Quisque in lectus leo. Aenean libero nunc, rutrum quis velit vel, tristique vulputate magna. Sed et lorem et lectus tempus dignissim.',
-              date_purchased: firebase.firestore.Timestamp.fromDate(new Date("December 26, 1777")),
-            })
-          }
-      }
       async componentDidMount(){
-      // STUFF YOU DO RIGHT AT THE BEGINING 
+        axios.defaults.headers.post['Content-Type'] = 'application/json';
+        axios.defaults.headers.post['Authorization'] = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwcm9kLXVzZXItY2xpZW50OmVhYWxsZW4iLCJpc3MiOiJhZ2VudDplYWFsbGVuOjo0YzBlYWQ5YS1kODE5LTQzMWMtYjVmOS0zNGEwZDE5MzRkOGQiLCJpYXQiOjE1Nzc3MTc5OTcsInJvbGUiOlsidXNlcl9hcGlfcmVhZCIsInVzZXJfYXBpX3dyaXRlIl0sImdlbmVyYWwtcHVycG9zZSI6dHJ1ZSwic2FtbCI6e319.XbV9G84LNvqN6RREjPKFlDLQrTtzUu5KVu46xDS7TOtGnMZ94h1PrNaAkQ6zT-79QOM7Ku2GrZdivguQ_o9jsw';
+        axios.defaults.url = "https://api.data.world/v0/sql/eaallen/lodestone"
+        axios.defaults.method= 'post' 
+        let rater_most_correct_3 = await axios({data:{query: "select rater ,COUNT(rater_correct_3) from out WHERE rater_correct_3 = true group by rater"}})
+        let rater_most_correct_5 = await axios({data:{query: "SELECT rater ,COUNT(rater_correct_5) FROM out WHERE rater_correct_5 = true GROUP BY rater"}})
+        let most_task_complete = await axios({data:{query: "SELECT rater ,COUNT(task_id) FROM out GROUP BY rater"}})
+        let perc_5_true = await axios({data:{query: "SELECT out.correct_answer_5 ,COUNT(out.rater_correct_5) FROM out WHERE out.rater_correct_5 = true GROUP BY out.correct_answer_5"}})
+        let perc_5_all = await axios({data:{query: "SELECT out.correct_answer_5 ,COUNT(out.rater_correct_5) FROM out GROUP BY out.correct_answer_5"}})
+        let perc_3_true = await axios({data:{query: "SELECT out.correct_answer_3 ,COUNT(out.rater_correct_3) FROM out WHERE out.rater_correct_3 = true GROUP BY out.correct_answer_3"}})
+        let perc_3_all = await axios({data:{query: "SELECT out.correct_answer_3 ,COUNT(out.rater_correct_3) FROM out GROUP BY out.correct_answer_3"}})
+        this.setState({...this.state,
+          a_rater_most_correct_3:rater_most_correct_3.data,
+          a_rater_most_correct_5:rater_most_correct_5.data,
+          a_most_task_complete:most_task_complete.data,
+          a_perc_5_true: perc_5_true.data,
+          a_perc_5_all: perc_5_all.data,
+          a_perc_3_true: perc_3_true.data,
+          a_perc_3_all: perc_3_all.data,
+
+        })
+        // console.log('response from data.world',perc_3_all.data)
       }
         render(){
+          console.log('CONTEXT STATE', this.state)
           return(
             <AppContext.Provider value={{...this.state, ...this.actions }}>
               {this.props.children}
